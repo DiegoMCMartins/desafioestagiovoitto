d25081d54edfbff466bc1fca6204ecf373fc48a5
DiegoMCMartins <diegomcastro2.8@gmail.com>
2021-06-28 15:10:56 -0300

    add cursos routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,13 +2,19 @@ import { Router } from 'express';
 
 /** Controllers */
 import AlunosController from '../app/controllers/AlunoController';
+import CursosController from '../app/controllers/CursoController';
 /**  * */
 
 const routes = new Router();
-
+/** Aluno routes */
 routes.get('/alunos', AlunosController.index);
 routes.post('/aluno', AlunosController.read);
 routes.post('/novoAluno', AlunosController.create);
 routes.post('/excluirAluno', AlunosController.delete);
+/** */
+
+/** Curso routes */
+routes.get('/cursos', CursosController.index);
+/** */
 
 export default routes;
