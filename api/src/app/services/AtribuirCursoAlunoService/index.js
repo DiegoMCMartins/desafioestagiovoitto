bfb10869c88e3309c74577e94be8834264dfa084
DiegoMCMartins <diegomcastro2.8@gmail.com>
2021-06-30 11:14:51 -0300

    add remove on atribuir curso aluno service

--- api/src/app/services/AtribuirCursoAlunoService/index.js
+++ api/src/app/services/AtribuirCursoAlunoService/index.js
@@ -6,6 +6,12 @@ class AtribuirCursoAlunoService {
     await aluno.addCurso(id_curso);
     return true;
   }
+
+  async remove({id_aluno, id_curso}) {
+    const aluno = await Aluno.findByPk(id_aluno);
+    await aluno.removeCurso(id_curso);
+    return true;
+  }
 }
 
 export default new AtribuirCursoAlunoService();
